176cdd07168c1a2cabb804b15802aee0f4182624
Western8 <westernchrome@gmail.com>
2023-02-22 22:14:59 +0300

    feat: implement class guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.res = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let res = this.min + Math.round((this.max - this.min) / 2);
+        this.res = res;
+        return res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
